8bd77f980ed64ae39395248b5854d137742acbdd
sushilkumar-sharma-aot <sushilkumar.sharma@algoocean.com>
2025-09-19 09:52:08 +0530

    new line added

--- src/components/test1.tsx
+++ src/components/test1.tsx
@@ -3,6 +3,7 @@ import React from "react";
 function test1() {
   function function2() {
     console.log("this is from main branch");
+    console.log("this is new line in main branch");
   }
   return <div>test1</div>;
 }
